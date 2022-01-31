44c607b60b3f8b606f7048a58510bdeadf867bc3
jenmlucas <jenmlucas1@gmail.com>
2022-01-31 16:21:47 -0700

    correct about page to be individual components rather then a card

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,53 +1,72 @@
 import React from "react";
 import placeholder from "../../assets/images/me/placeholder.png";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { makeStyles } from "@mui/styles";
+import Avatar from "@mui/material/Avatar";
 
+const useStyles = makeStyles({
+  aboutMeHeader: {
+    textAlign: "center",
+  },
+  about: {
+    display: "flex",
+    alignItems: "center",
+    marginLeft: 500,
+  },
+  bio: {
+    width: "40%",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+});
 
 function About() {
-
+  const terms = useStyles();
   return (
-    <Card  Box sx={{ display: 'flex', ml: 20, mr: 20 }}>
-      <CardMedia
-        component="img"
-        height="400"
-        image={placeholder}
-        alt="me"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" Box sx={{ textAlign: 'center', m: 3 }}>
-            About Me
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lorem ipsum dolor sit amet, atqui nominati disputationi eu vel, vel
-            nulla iudico conclusionemque id. Eu vix nostro minimum. Sea quis
-            atomorum ad, ut lorem sapientem hendrerit mei. Qui tale platonem eu,
-            tamquam periculis rationibus pro ne. Cum purto dolores ad, cu falli
-            delectus disputationi mea. An vis graeci commodo, mei at sale iusto
-            complectitur. Dicit integre qui ut, his ut dolorum mediocrem. Option
-            volumus eum in, vidit democritum disputationi vel cu. Consul soleat
-            pri id, id decore ignota nec. Sit vitae deseruisse ea, qui cu inermis
-            apeirian dissentiunt. Odio molestie corrumpit mea et, libris equidem
-            temporibus in nec. Est tota ferri et. Elit eligendi ei quo. Esse
-            vituperata vel at, offendit maiestatis ne vix, qui dicunt impetus at.
-            Has nibh doming mnesarchum an, erat graeci sed et. His latine ancillae
-            ad, has vero virtute volumus ad. Mei prima omittam patrioque no, eu
-            vel omittam sapientem, pro an illud assum. Autem accusam qui eu, id
-            partem audire quaerendum sed. Ei vel falli volumus disputando, ut eius
-            ignota nostrum has. Est cu veri probo ubique, no probo error mollis
-            pri, quo falli tacimates at. Vix modus audiam scaevola no, eos ex
-            vitae quaestio qualisque.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button href="https://github.com/jenmlucas" target= "blank" size="small">Github</Button>
-          <Button href="https://www.linkedin.com/in/jennifer-lucas-478599218/" target= "blank" size="small">LinkedIN</Button>
-        </CardActions>
-      </Card>
+    <section>
+      <div className={terms.about}>
+        <Avatar
+          src={placeholder}
+          sx={{ width: 400, height: 400 }}
+          alt="Jennifer Lucas"
+          variant="square"
+        ></Avatar>
+
+        <p className={terms.bio}>
+          <h1 className={terms.aboutMeHeader}> About Me </h1>
+          Full- stack web developer with a background in Medical, which has
+          instilled strengths in meeting deadlines, organization, creativity,
+          learning quickly on the job, and communication with teamwork. Working
+          towards earning a certificate from the University of Arizona through a
+          coding bootcamp, with newly developed skills in HTML, CSS, JavaScript,
+          React.js, MySql, and responsive web design. I worked on a team project
+          to create an interactive application that allows you to search for
+          National parks near your location while also displaying your local
+          weather using your IP address. My abilities to communicate along with
+          my leadership skills, makes me a valuable addition to any team.
+          Passionate about developing responsive websites that haven’t been seen
+          before for potential clients. <br></br>
+          <br></br>
+          <Button
+            href="https://github.com/jenmlucas"
+            target="blank"
+            size="small"
+          >
+            Github
+          </Button>
+          <Button
+            href="https://www.linkedin.com/in/jennifer-lucas-478599218/"
+            target="blank"
+            size="small"
+          >
+            LinkedIN
+          </Button>
+          <Button href="#" target="blank" size="small">
+            Resume
+          </Button>
+        </p>
+      </div>
+    </section>
   );
 }
 
